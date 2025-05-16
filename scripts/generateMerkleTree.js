8cc103b968083743caf5115d888d38bb6f589413
Freddie Nguyen <nguyenbs@gmail.com>
2025-05-16 16:53:44 +0700

    Update Merkle tree testdata

--- scripts/generateMerkleTree.js
+++ scripts/generateMerkleTree.js
@@ -3,46 +3,54 @@ const fs = require("fs");
 
 const whitelistByTiers = {
     1: [
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
-        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "1"],
-        ["0x3187b84013078f714e1486be6ab624716eec5465", "1"],
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],
-        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "1"],
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "100"],
+        ["0x3187b84013078f714e1486be6ab624716eec5465", "100"],
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "100"],
+        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "100"],
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],        
     ],
     2: [
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
-        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "1"],
-        ["0x3187b84013078f714e1486be6ab624716eec5465", "1"],
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],
-        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "1"],   
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "100"],
+        ["0x3187b84013078f714e1486be6ab624716eec5465", "100"],
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "100"],
+        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "100"],   
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     3: [
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
-        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "1"],
-        ["0x3187b84013078f714e1486be6ab624716eec5465", "1"],
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],   
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "10"],
+        ["0x3187b84013078f714e1486be6ab624716eec5465", "10"],
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "10"],   
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     4: [
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
-        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "1"],
-        ["0x3187b84013078f714e1486be6ab624716eec5465", "1"],   
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0xeec07af430abbcafb246c8b8ff22f2950215f519", "100"],
+        ["0x3187b84013078f714e1486be6ab624716eec5465", "100"],   
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     5: [
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],
-        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "1"],   
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "100"],
+        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "100"],   
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     6: [
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "100"],
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     7: [
-        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "1"],
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
+        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "100"],
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ],
     8: [
-        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "1"],
-        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "1"],
-        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "1"],
+        ["0x51b0b6418b910c97fbf618bb6f273a1c5a60ee87", "100"],
+        ["0x308a9c078f843dbb7e8826b50b485b01f62a0ca7", "100"],
+        ["0xabde77f54a80b597eb909b31a4c219e83df7a197", "100"],
+        ["0x99c8A321DA02F71e8fb87d5B135312dA060D3c35", "100"],    
     ]
 }
 
@@ -54,6 +62,8 @@ async function main() {
     const { configContract, iusdtAddress } = nodeSaleContracts;
     delete nodeSaleContracts.configContract
     delete nodeSaleContracts.iusdtAddress
+
+    console.log(`ConfigContract: `, JSON.stringify(configContract));
 
     let whitelistSale = {}
 
